c0bcfc2b6538cb32bc8f18b70fb34ee7cf8f5074
vero7bg7 <vero7bg7@hotmail.com>
2022-07-04 18:07:55 +0200

    añadido archivo City en models y routes

--- api-node/index.js
+++ api-node/index.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { connection } from './utils/db.js';
-
+import { connection } from '../api-node/utils/db.js';
 import { Character } from './models/Character.js';
+
 import { characterRoutes } from './routes/character.routes.js';
+import { cityRoutes } from "./routes/city.routes.js";
 
 //SERVER
 const PORT = process.env.PORT || 3000;
@@ -21,10 +22,11 @@ server.use("/", router);
 
 //Routes
 server.use('/characters', characterRoutes);
+server.use('/cities', cityRoutes);
 
 //Control de errores//se crea un error para cuando no encuentre la ruta
 server.use('*', (req, res, next) => {
-  const error = new Error('Route not found'); 
+  const error = new Error('Route not found');
   error.status = 404;
   next(error); // Lanzamos la función next() con un error
 });
